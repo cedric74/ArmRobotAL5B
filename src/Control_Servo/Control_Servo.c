93a1b744a4dc4ba8cd8510d61ebae5f6ca18bd33
Cedric Toncanier <cedric.toncanier@gmail.com>
2016-03-02 10:29:43 -0500

    Create Tab Servo

--- src/Control_Servo/Control_Servo.c
+++ src/Control_Servo/Control_Servo.c
@@ -18,10 +18,27 @@
 *   T Y P E D E F   &  C O N S T A N T E   *
 ********************************************/
 #define		DELAYS_500_MS				500000	// in us,
+#define		DELAYS_1_S				   1000000	// in us,
 #define		DELAYS_5_S				   5000000	// in us,
 
-// Example Pince
-ePwm servoM1 =  	PWM_P8_13;
+#define    NUMBER_SERVO					5
+
+typedef struct{
+	ePwm pwm;
+	int iMin;
+	int iMax;
+	int iAx;
+	int iB;
+}sServo;
+
+sServo sTabServo[]={
+//			     Pwm, Min Val,  Max Value,    Ax,      B
+/* M0 */{  PWM_P8_13,       0,			0,     0,      0},
+/* M1 */{  PWM_P8_13,  625000,    2250000, 	9027, 629167},
+/* M2 */{  PWM_P8_13,       0,			0,     0,      0},
+/* M3 */{  PWM_P8_13,       0,			0,     0,      0},
+/* M4 */{  PWM_P8_13,       0,			0,     0,      0},
+};
 
 // m1   00D = 2250000
 // m1   90D = 1450000
@@ -33,7 +50,7 @@ ePwm servoM1 =  	PWM_P8_13;
 /*******************************************
 *	        F U N C T I O N S   	       *
 ********************************************/
-static int Control_Servo_Calcul(int iAngle);
+int Control_Servo_GetPulseValue(eServo servo, int iAngle);
 
 /*
  ============================================
@@ -46,7 +63,7 @@ static int Control_Servo_Calcul(int iAngle);
 void Control_Servo_init(){
 
 	// Init Servo Pince
-	Lib_Servo_init(servoM1);
+	Lib_Servo_init(sTabServo[Servo_M1].pwm);
 }
 
 /*
@@ -59,56 +76,38 @@ void Control_Servo_init(){
  */
 void Control_Servo_Rotate(){
 
-	// Turn Servo to Center Position
-	Lib_pwm_control(servoM1, PERIOD_20_MS, 1450000);
-
-	usleep(DELAYS_500_MS);
-
-	Lib_pwm_control(servoM1, PERIOD_20_MS, 2250000);
-
-	usleep(DELAYS_500_MS);
-
-	Lib_pwm_control(servoM1, PERIOD_20_MS, 1450000);
-
-
-	usleep(DELAYS_500_MS);
-
-	Lib_pwm_control(servoM1, PERIOD_20_MS, 625000);
-
-	usleep(DELAYS_500_MS);
-
-
-	Lib_pwm_control(servoM1, PERIOD_20_MS, 1450000);
-//
-	// ------
-//	Lib_pwm_control(servoM1, PERIOD_20_MS, Control_Servo_Calcul(90));
-//
-//	usleep(DELAYS_500_MS);
-//
-//	Lib_pwm_control(servoM1, PERIOD_20_MS, Control_Servo_Calcul(0));
-//
-//	usleep(DELAYS_500_MS);
-//
-//	Lib_pwm_control(servoM1, PERIOD_20_MS, Control_Servo_Calcul(90));
-//
-//	usleep(DELAYS_500_MS);
-//
-//	Lib_pwm_control(servoM1, PERIOD_20_MS, Control_Servo_Calcul(180));
+	// Test Servo 1
+	int iCurrentAngle = 0;
+	Lib_pwm_control(sTabServo[Servo_M1].pwm, PERIOD_20_MS, Control_Servo_GetPulseValue(sTabServo[Servo_M1].pwm, iCurrentAngle));
+	usleep(DELAYS_1_S);
+	int i = 0;
+	for(i = 0; iCurrentAngle<= 180; i++){
+		Lib_pwm_control(sTabServo[Servo_M1].pwm, PERIOD_20_MS, Control_Servo_GetPulseValue(sTabServo[Servo_M1].pwm, iCurrentAngle));
+		iCurrentAngle += 10;
+		usleep(DELAYS_500_MS);
+	};
 
 }
 
 /*
  ============================================
- Function     : Control_Servo_Calcul()
+ Function     : Control_Servo_GetPulseValue()
  Parameter    :
  Return Value : void
  Description  :
  ============================================
  */
-static int Control_Servo_Calcul(int iAngle){
-	// y = 812500x - 183333
-	int iReturn = ((812500 * iAngle) - 183333);
-	printf("Value = %i\n", iReturn);
+int Control_Servo_GetPulseValue(eServo servo, int iAngle){
+
+	int iReturn = ((sTabServo[Servo_M1].iAx * iAngle) + sTabServo[Servo_M1].iB);
+	printf("Value = %i , Angle: %i\n", iReturn, iAngle);
+
+	if( iReturn< sTabServo[Servo_M1].iMin){
+		return sTabServo[Servo_M1].iMin;
+	}
+	if(iReturn > sTabServo[Servo_M1].iMax){
+		return sTabServo[Servo_M1].iMax;
+	}
 	return iReturn;
 
 }
